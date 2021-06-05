4695abaad642764e5e0e90155abf3d87d70c49f4
Varun Khalate <khalatevarun@gmail.com>
2021-06-06 02:10:11 +0530

    load dummy content dynamically

--- src/components/section2/Section2.js
+++ src/components/section2/Section2.js
@@ -1,4 +1,20 @@
+import { steps } from '../../data/data';
+
 const Section2 = () => {
-  return <div>Hello World</div>;
+  const process = steps.map((s) => (
+    <div>
+      <div>{s.no}</div>
+      <div>{s.text}</div>
+      <div>
+        <img src={s.img} alt={s.no} />
+      </div>
+    </div>
+  ));
+  return (
+    <div>
+      <div className="title">How it Works</div>
+      {process}
+    </div>
+  );
 };
 export default Section2;
