baa415e894e2ef8bb26f4d1c8d4c1b8ce38aa8ea
Varun Khalate <khalatevarun@gmail.com>
2021-06-06 17:00:47 +0530

    add started files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Hero from './components/hero-section/Hero';
 import Navbar from './components/navbar/Navbar';
 import Section2 from './components/section2/Section2';
 import Section3 from './components/section3/Section3';
+import Section4 from './components/section4/Section4';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <Section2 />
       <Section3 />
+      <Section4 />
     </div>
   );
 };
---
+++ src/components/section4/Section4.js
@@ -0,0 +1,4 @@
+const Section4 = () => {
+  return <div>Hello World</div>;
+};
+export default Section4;
