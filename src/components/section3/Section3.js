4189f17be2f764870460aae923ffc865a6d52803
Varun Khalate <khalatevarun@gmail.com>
2021-06-06 15:41:55 +0530

    add dekstop and mobile styling to the section

--- src/components/section3/Section3.js
+++ src/components/section3/Section3.js
@@ -1,20 +1,21 @@
 import { partners } from '../../data/data';
+import './Section3.css';
 
 const Section3 = () => {
   const Partnerships = partners.map((partner, i) => (
-    <div className="Partners">
-      <img src={partner} alt={i} />
+    <div className="Images">
+      <img src={partner} alt={i} width="150px" />
     </div>
   ));
 
   return (
     <div className="Section3">
-      <div className="Title">Our Partnerships</div>
+      <div className="Head">Our Partnerships</div>
       <div className="Descrp">
         We have partnered with the best hospitals and doctors in Australia to
         provide you the best healthcare.
       </div>
-      <div>{Partnerships}</div>
+      <div className="Partners">{Partnerships}</div>
     </div>
   );
 };
