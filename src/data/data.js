dbe084a7fbfae9b5a3fe652ac06a0fb665105a24
Varun Khalate <khalatevarun@gmail.com>
2021-06-07 11:14:38 +0530

    add data

--- src/data/data.js
+++ src/data/data.js
@@ -9,6 +9,10 @@ import indraprastha from '../images/indraprastha.png';
 import fortis from '../images/fortis.png';
 import blk from '../images/blk.png';
 import artemis from '../images/artemis.png';
+import tip1 from '../images/tip1.png';
+import tip2 from '../images/tip2.png';
+import tip3 from '../images/tip3.png';
+import tip4 from '../images/tip4.png';
 
 export const categories = [
   { img: dentist, name: 'Dentist' },
@@ -20,3 +24,62 @@ export const categories = [
 ];
 
 export const partners = [max, indraprastha, fortis, blk, artemis];
+
+export const tips = [
+  {
+    img: tip1,
+    date: '28th May 2021',
+    title: 'Eat fresh Fruits and vegetable Daily',
+    summary:
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.',
+  },
+  {
+    img: tip2,
+    date: '28th May 2021',
+    title: 'Eat fresh Fruits and vegetable Daily',
+    summary:
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.',
+  },
+  {
+    img: tip3,
+    date: '28th May 2021',
+    title: 'Eat fresh Fruits and vegetable Daily',
+    summary:
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.',
+  },
+  {
+    img: tip4,
+    date: '28th May 2021',
+    title: 'Eat fresh Fruits and vegetable Daily',
+    summary:
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.',
+  },
+  {
+    img: tip1,
+    date: '28th May 2021',
+    title: 'Eat fresh Fruits and vegetable Daily',
+    summary:
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.',
+  },
+  {
+    img: tip2,
+    date: '28th May 2021',
+    title: 'Eat fresh Fruits and vegetable Daily',
+    summary:
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.',
+  },
+  {
+    img: tip3,
+    date: '28th May 2021',
+    title: 'Eat fresh Fruits and vegetable Daily',
+    summary:
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.',
+  },
+  {
+    img: tip4,
+    date: '28th May 2021',
+    title: 'Eat fresh Fruits and vegetable Daily',
+    summary:
+      'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.',
+  },
+];
