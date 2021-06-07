f62d97ef949200e9af8f597a3fc54a8434a52228
Varun Khalate <khalatevarun@gmail.com>
2021-06-07 15:48:03 +0530

    add content without styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,43 @@
+import { Button } from '@material-ui/core';
+
 const Footer = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <p>SmartMeet</p>
+          <p>Be sure to take a look at our Terms of Use and Privacy Policy</p>
+        </div>
+        <div>
+          <p>About</p>
+          <p>About Us</p>
+          <p>Blog</p>
+          <p>Team</p>
+          <p>Career</p>
+          <p>Contact</p>
+        </div>
+        <div>
+          <p>Company</p>
+          <p>Privacy</p>
+          <p>Support</p>
+          <p>Help Desk</p>
+          <p>FAQ</p>
+        </div>
+        <div>
+          <p>Book an Appointment</p>
+          <Button></Button>
+          <Button>Book Appointment</Button>
+          <div>
+            <img />
+            <img />
+            <img />
+          </div>
+        </div>
+        <div>Horizontal Line</div>
+        <div>Copyright2021 Redien.Design</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
