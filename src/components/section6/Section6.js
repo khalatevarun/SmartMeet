e0f6921e4136d3c0452a3c2ed6384670e9dc60c3
Varun Khalate <khalatevarun@gmail.com>
2021-06-07 11:41:42 +0530

    complete desktop design

--- src/components/section6/Section6.js
+++ src/components/section6/Section6.js
@@ -1,18 +1,24 @@
 import { tips } from '../../data/data';
+import './Section6.css';
 
 const Section6 = () => {
   const advices = tips.map((tip) => (
-    <div>
-      <div>
-        <img src={tip.img} alt={tip.title} />
+    <div className="Single">
+      <div className="Pic">
+        <img src={tip.img} alt={tip.title} height="200px" />
       </div>
-      <div>{tip.date}</div>
-      <div>{tip.title}</div>
-      <div>{tip.summary}</div>
+      <div className="Date">{tip.date}</div>
+      <div className="TipTitle">{tip.title}</div>
+      <div className="TipSummary">{tip.summary}</div>
     </div>
   ));
 
-  return <div>{advices}</div>;
+  return (
+    <div className="Section6">
+      <div className="HealthTitle">Health tips and Advise</div>
+      <div className="TipsWrapper">{advices}</div>
+    </div>
+  );
 };
 
 export default Section6;
