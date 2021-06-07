e79ce9726a2724c129f6e686ff8b45fa3a60d274
Varun Khalate <khalatevarun@gmail.com>
2021-06-07 19:50:45 +0530

    add avatars and increase margin of navbar

--- src/components/hero-section/Hero.js
+++ src/components/hero-section/Hero.js
@@ -1,9 +1,6 @@
 import './Hero.css';
-import dentist from '../../images/dentist.png';
 import {
-  Avatar,
   Button,
-  Input,
   InputAdornment,
   InputBase,
   withStyles,
@@ -14,6 +11,9 @@ import Carousel from './Carousel';
 import CircleAvatars from './CircleAvatars';
 import hero1 from '../../images/hero1.png';
 import hero2 from '../../images/hero2.png';
+import hero4 from '../../images/hero4.png';
+
+import hero3 from '../../images/hero3.png';
 
 const Hero = () => {
   const AppointmentButton = withStyles((theme) => ({
@@ -35,50 +35,63 @@ const Hero = () => {
   return (
     <div className="Hero">
       <div className="Highlight">
-        <CircleAvatars img={hero1} />
+        <img
+          src={hero1}
+          height="150px"
+          style={{ position: 'absolute', left: '40px', top: '250px' }}
+          alt="hero1"
+        />
         <p>
           Book appointment for{' '}
           <span style={{ color: '#e94a90' }}>you and your family</span> with
           qualified doctors online
         </p>
-        <CircleAvatars img={hero2} />
+        <img
+          src={hero2}
+          height="200px"
+          style={{ position: 'absolute', right: '10px', top: '180px' }}
+          alt="hero2"
+        />
       </div>
 
       <Carousel />
-
-      <div className="Location">
-        <div class="Inputs">
-          <div className="locationInput">
-            <InputBase
-              placeholder="Location"
-              startAdornment={
-                <InputAdornment position="start">
-                  <LocationOnIcon color="disabled" />
-                </InputAdornment>
-              }
-            />
+      <div className="Bottom">
+        <img src={hero3} height="200px" alt="hero1" />
+        <div className="Location">
+          <div class="Inputs">
+            <div className="locationInput">
+              <InputBase
+                placeholder="Location"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <LocationOnIcon color="disabled" />
+                  </InputAdornment>
+                }
+              />
+            </div>
+            <div className="verticalLine">
+              <hr
+                size="50"
+                color="#32177c"
+                style={{ width: '1px', border: 'none' }}
+              ></hr>
+            </div>
+            <div className="locationType">
+              <InputBase
+                placeholder="Doctor, speciality, hospital or treatment"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <MyLocationIcon color="disabled" />
+                  </InputAdornment>
+                }
+              />
+            </div>
           </div>
-          <div className="verticalLine">
-            <hr
-              size="50"
-              color="#32177c"
-              style={{ width: '1px', border: 'none' }}
-            ></hr>
-          </div>
-          <div className="locationType">
-            <InputBase
-              placeholder="Doctor, speciality, hospital or treatment"
-              startAdornment={
-                <InputAdornment position="start">
-                  <MyLocationIcon color="disabled" />
-                </InputAdornment>
-              }
-            />
-          </div>
+          <AppointmentButton variant="contained" disableElevation color="#fff">
+            Book Now
+          </AppointmentButton>
         </div>
-        <AppointmentButton variant="contained" disableElevation color="#fff">
-          Book Now
-        </AppointmentButton>
+        <img src={hero4} height="180px" alt="hero1" />
       </div>
     </div>
   );
