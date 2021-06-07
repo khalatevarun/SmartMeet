78cc69171afd72ddc9efb2c676064808f6ddbc54
Varun Khalate <khalatevarun@gmail.com>
2021-06-07 14:33:28 +0530

    complete desktop design

--- src/components/section8/Section8.js
+++ src/components/section8/Section8.js
@@ -1,5 +1,29 @@
+import apple_store from '../../images/apple_store.png';
+import play_store from '../../images/play_store.png';
+import mobile from '../../images/mobile.png';
+import './Section8.css';
+
 const Section8 = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="Section8">
+      <div className="Div1">
+        <div
+          className="TitleBookings
+        "
+        >
+          For faster bookings,
+          <br /> Use Smart Meet App
+        </div>
+        <div>
+          <img src={play_store} alt="play store" style={{ marginRight: 30 }} />
+          <img src={apple_store} alt="apple store" />
+        </div>
+      </div>
+      <div className="Div2">
+        <img src={mobile} alt="mobiles" width="1000px" />
+      </div>
+    </div>
+  );
 };
 
 export default Section8;
